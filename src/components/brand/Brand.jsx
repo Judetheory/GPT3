25992469609b3ed1394d66a68f690453c3d63fa8
Jude Tochukwu Ogbozor <judtheory@gmail.com>
2024-06-20 16:26:30 +0100

    Header Component

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './brand.css'
-
+import {
+google,slack,atlassian,dropbox,shopify
+} from './imports'
+import './brand.css'
 const Brand = () => {
   return (
     <div>
